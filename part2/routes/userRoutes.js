f5aff98ad6ceb42130d3d39275ff11422280b355
Enze <141884428+Enzeef4f3qg@users.noreply.github.com>
2025-06-20 07:14:51 +0000

    [automatic commit] 2025-06-20 07:14:50 UTC 58.174.28.202

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -84,7 +84,7 @@ router.post('/login', async (req, res) => {
 // routes/users.js
 router.post('/logout', (req, res) => {
   req.session.destroy(() => {
-    res.clearCookie('continueCode'); //
+    res.clearCookie('continueCode');
     res.json({ message: 'Logged out' });
   });
 });
