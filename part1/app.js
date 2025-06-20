b9bea83db25320d8f50c815ef5e7ac331065eedd
Enze <141884428+Enzeef4f3qg@users.noreply.github.com>
2025-06-20 03:21:34 +0000

    [automatic commit] 2025-06-20 03:21:34 UTC 58.174.28.202

--- part1/app.js
+++ part1/app.js
@@ -62,7 +62,7 @@ let db;
         request_id INT NOT NULL,
         walker_id INT NOT NULL,
         applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        status ENUM('pending', 'accepted', 'rejected') DEFAULT 'pending',
+        status ENUM('open', 'accepted', 'completed', 'cancelled') DEFAULT 'pending',
         FOREIGN KEY (request_id) REFERENCES WalkRequests(request_id),
         FOREIGN KEY (walker_id) REFERENCES Users(user_id),
         CONSTRAINT unique_application UNIQUE (request_id, walker_id)
