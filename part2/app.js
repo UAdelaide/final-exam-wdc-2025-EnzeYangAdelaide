18657c1db51299886cf461e22822ceda9d939cec
Enze <141884428+Enzeef4f3qg@users.noreply.github.com>
2025-06-20 14:09:49 +0000

    [automatic commit] 2025-06-20 14:09:49 UTC 58.174.28.202

--- part2/app.js
+++ part2/app.js
@@ -22,11 +22,11 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
-const dogsRouter = require('./routes/dogRoutes');
+const dogsRouter = require('./routes/dogRoutes');//add dogroutes
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
-app.use('/api/dogs', dogsRouter);
+app.use('/api/dogs', dogsRouter);//follow above
 
 
 // Export the app instead of listening here
