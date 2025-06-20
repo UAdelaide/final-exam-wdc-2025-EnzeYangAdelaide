efa1078d839e4c0d05872c18b2062d9b3912fe1e
Enze <141884428+Enzeef4f3qg@users.noreply.github.com>
2025-06-20 07:37:41 +0000

    [automatic commit] 2025-06-20 07:37:41 UTC 58.174.28.202

--- part2/app.js
+++ part2/app.js
@@ -6,10 +6,10 @@ const app = express();
 const session = require('express-session');
 
 app.use(session({
-  secret: 'yourSecretKey', // 换成你自己的密钥
+  secret: '',
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false } // 开发环境下 secure 要为 false
+  cookie: { secure: false }
 }));
 
 // Middleware
