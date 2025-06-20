66ddf06ad0c9017400a855fa7697aaad44f635f7
Enze <141884428+Enzeef4f3qg@users.noreply.github.com>
2025-06-20 03:23:17 +0000

    [automatic commit] 2025-06-20 03:23:17 UTC 58.174.28.202

--- part1/app.js
+++ part1/app.js
@@ -124,7 +124,7 @@ let db;
         ((SELECT dog_id FROM Dogs WHERE name='Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
         ((SELECT dog_id FROM Dogs WHERE name='Charlie'), '2025-06-11 10:00:00', 60, 'Morialta Park', 'open'),
         ((SELECT dog_id FROM Dogs WHERE name='Lucy'), '2025-06-12 14:00:00', 30, 'Botanic Garden', 'open'),
-        ((SELECT dog_id FROM Dogs WHERE name='Rocky'), '2025-06-13 16:30:00', 20, 'Mawson Lakes', 'pending')
+        ((SELECT dog_id FROM Dogs WHERE name='Rocky'), '2025-06-13 16:30:00', 20, 'Mawson Lakes', 'open')
       `);
 
       // 插入 WalkRatings，假设已完成的walks请求id为2,3，walker为bobwalker
